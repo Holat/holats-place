6c742a2caf8df9dda2a489da81fea8992e2d7be5
Holat <105239299+Holat@users.noreply.github.com>
2024-06-19 15:22:31 +0100

    Create generateTransactionRef.ts

---
+++ services/generateTransactionRef.ts
@@ -0,0 +1,17 @@
+export default generateTransactionRef(length: number){
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+  
+    const dateTimeString = `${year}${month}${day}${hours}${minutes}${seconds}`;
+    const randomComplexNumber = Array.from({ length: 16 }, () => {
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        return chars.charAt(Math.floor(Math.random() * chars.length));
+    }).join('');
+    const result = `${randomComplexNumber}${dateTimeString}`;
+    return `flw_tx_ref_${result}`;
+  };
